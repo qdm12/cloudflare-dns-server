mm97mm/dns

Initial code transition

cmd/main.go
@@ -17,13 +17,13 @@ import (
 	"github.com/qdm12/dns/internal/splash"
 	"github.com/qdm12/dns/pkg/blacklist"
 	"github.com/qdm12/dns/pkg/check"
+	"github.com/qdm12/dns/pkg/doh"
+	"github.com/qdm12/dns/pkg/dot"
 	"github.com/qdm12/dns/pkg/nameserver"
-	"github.com/qdm12/dns/pkg/unbound"
 	"github.com/qdm12/golibs/logging"
-	customOS "github.com/qdm12/golibs/os"
-	"github.com/qdm12/updated/pkg/dnscrypto"
 )
 
+//nolint:gochecknoglobals
 var (
 	version   string
 	buildDate string
@@ -43,11 +43,10 @@ func main() {
 	args := os.Args
 	logger := logging.NewParent(logging.Settings{})
 	configReader := config.NewReader(logger)
-	osIntf := customOS.New()
 
 	errorCh := make(chan error)
 	go func() {
-		errorCh <- _main(ctx, buildInfo, args, logger, configReader, osIntf)
+		errorCh <- _main(ctx, buildInfo, args, logger, configReader)
 	}()
 
 	select {
@@ -78,8 +77,7 @@ func main() {
 }
 
 func _main(ctx context.Context, buildInfo models.BuildInformation,
-	args []string, logger logging.ParentLogger, configReader config.Reader,
-	os customOS.OS) error {
+	args []string, logger logging.ParentLogger, configReader config.Reader) error {
 	if health.IsClientMode(args) {
 		// Running the program in a separate instance through the Docker
 		// built-in healthcheck, in an ephemeral fashion to query the
@@ -97,30 +95,14 @@ func _main(ctx context.Context, buildInfo models.BuildInformation,
 
 	const clientTimeout = 15 * time.Second
 	client := &http.Client{Timeout: clientTimeout}
-	// Create configurators
-	dnsCrypto := dnscrypto.New(client, "", "") // TODO checksums for build
-	const unboundEtcDir = "/unbound"
-	const unboundPath = "/unbound/unbound"
-	const cacertsPath = "/unbound/ca-certificates.crt"
-	dnsConf := unbound.NewConfigurator(logger, os.OpenFile, dnsCrypto, unboundEtcDir, unboundPath, cacertsPath)
-
-	if len(args) > 1 && args[1] == "build" {
-		if err := dnsConf.SetupFiles(ctx); err != nil {
-			return err
-		}
-		return nil
-	}
-
-	version, err := dnsConf.Version(ctx)
-	if err != nil {
-		return err
-	}
-	logger.Info("Unbound version: %s", version)
 
 	settings, err := configReader.ReadSettings()
 	if err != nil {
 		return err
 	}
+	logger = logger.NewChild(logging.Settings{
+		Level: settings.LogLevel,
+	})
 	logger.Info("Settings summary:\n" + settings.String())
 
 	wg := &sync.WaitGroup{}
@@ -136,9 +118,9 @@ func _main(ctx context.Context, buildInfo models.BuildInformation,
 
 	localIP := net.IP{127, 0, 0, 1}
 	logger.Info("using DNS address %s internally", localIP.String())
-	nameserver.UseDNSInternally(localIP) // use Unbound
+	nameserver.UseDNSInternally(localIP) // use the DoT/DoH server
 	wg.Add(1)
-	go unboundRunLoop(ctx, wg, settings, logger, dnsConf, client, crashed)
+	go runLoop(ctx, wg, settings, logger, client, crashed)
 
 	select {
 	case <-ctx.Done():
@@ -149,8 +131,8 @@ func _main(ctx context.Context, buildInfo models.BuildInformation,
 	return err
 }
 
-func unboundRunLoop(ctx context.Context, wg *sync.WaitGroup, settings config.Settings, //nolint:gocognit
-	logger logging.Logger, dnsConf unbound.Configurator, client *http.Client, crashed chan<- error,
+func runLoop(ctx context.Context, wg *sync.WaitGroup, settings config.Settings,
+	logger logging.Logger, client *http.Client, crashed chan<- error,
 ) {
 	defer wg.Done()
 	defer logger.Info("unbound loop exited")
@@ -159,11 +141,9 @@ func unboundRunLoop(ctx context.Context, wg *sync.WaitGroup, settings config.Set
 	firstRun := true
 
 	var (
-		unboundCtx               context.Context
-		unboundCancel            context.CancelFunc
-		waitError                chan error
-		stdoutLines, stderrLines chan string
-		err                      error
+		serverCtx    context.Context
+		serverCancel context.CancelFunc
+		waitError    chan error
 	)
 
 	for ctx.Err() == nil {
@@ -172,12 +152,13 @@ func unboundRunLoop(ctx context.Context, wg *sync.WaitGroup, settings config.Set
 			timer.Reset(settings.UpdatePeriod)
 		}
 
+		serverSettings := dot.ServerSettings{
+			Resolver: settings.DoT.Resolver,
+			Port:     settings.DoT.Port,
+			Cache:    settings.DoT.Cache,
+		}
+
 		if !firstRun {
-			logger.Info("downloading DNSSEC root hints and named root")
-			if err := dnsConf.SetupFiles(ctx); err != nil {
-				logAndWait(ctx, logger, err)
-				continue
-			}
 			logger.Info("downloading and building DNS block lists")
 			blacklistBuilder := blacklist.NewBuilder(client)
 			blockedHostnames, blockedIPs, blockedIPNets, errs :=
@@ -188,97 +169,61 @@ func unboundRunLoop(ctx context.Context, wg *sync.WaitGroup, settings config.Set
 			logger.Info("%d hostnames blocked overall", len(blockedHostnames))
 			logger.Info("%d IP addresses blocked overall", len(blockedIPs))
 			logger.Info("%d IP networks blocked overall", len(blockedIPNets))
-			settings.Unbound.Blacklist = blacklist.Settings{
-				FqdnHostnames: blockedHostnames,
-				IPs:           blockedIPs,
-				IPNets:        blockedIPNets,
-			}
-		}
-
-		logger.Info("generating Unbound configuration")
-		if err := dnsConf.MakeUnboundConf(settings.Unbound); err != nil {
-			logAndWait(ctx, logger, err)
-			continue
+			serverSettings.Blacklist.IPs = blockedIPs
+			serverSettings.Blacklist.IPNets = blockedIPNets
+			serverSettings.Blacklist.BlockHostnames(blockedHostnames)
 		}
 
 		if !firstRun {
-			unboundCancel()
+			serverCancel()
 			<-waitError
 			close(waitError)
-			close(stdoutLines)
-			close(stderrLines)
 		}
-		unboundCtx, unboundCancel = context.WithCancel(ctx)
+		serverCtx, serverCancel = context.WithCancel(ctx)
 
-		logger.Info("starting unbound")
-		stdoutLines, stderrLines, waitError, err = dnsConf.Start(unboundCtx, settings.Unbound.VerbosityDetailsLevel)
-		if err != nil {
-			crashed <- err
-			break
+		var server models.Server
+		switch settings.UpstreamType {
+		case config.DoT:
+			server = dot.NewServer(serverCtx, logger, serverSettings)
+		case config.DoH:
+			server = doh.NewServer(serverCtx, logger, settings.DoH)
 		}
 
-		go logUnboundStreams(logger, stdoutLines, stderrLines)
+		logger.Info("starting DNS server")
+		waitError = make(chan error)
+		go server.Run(serverCtx, waitError)
 
 		if settings.CheckDNS {
 			if err := check.WaitForDNS(ctx, net.DefaultResolver); err != nil {
 				crashed <- err
-				break
+				serverCancel()
+				return
 			}
 		}
 
 		if firstRun {
-			logger.Info("restarting Unbound the first time to get updated files")
+			logger.Info("restarting DNS server the first time to get updated files")
 			firstRun = false
 			continue
 		}
 
 		select {
 		case <-timer.C:
-			logger.Info("planned restart of unbound")
+			logger.Info("planned periodic restart of DNS server")
 		case <-ctx.Done():
 			if !timer.Stop() {
 				<-timer.C
 			}
-			logger.Warn("context canceled: exiting unbound run loop")
+			logger.Warn("context canceled: exiting DNS server run loop")
 		case waitErr := <-waitError:
 			close(waitError)
-			close(stdoutLines)
-			close(stderrLines)
 			if !timer.Stop() {
 				<-timer.C
 			}
 			crashed <- waitErr
-			unboundCancel()
+			serverCancel()
 			return
 		}
 	}
-	unboundCancel()
-}
-
-func logAndWait(ctx context.Context, logger logging.Logger, err error) {
-	const wait = 10 * time.Second
-	logger.Error("%s, retrying in %s", err, wait)
-	timer := time.NewTimer(wait)
-	select {
-	case <-timer.C:
-	case <-ctx.Done():
-		if !timer.Stop() {
-			<-timer.C
-		}
-	}
-}
-
-func logUnboundStreams(logger logging.Logger, stdout, stderr <-chan string) {
-	var line string
-	var ok bool
-	for {
-		select {
-		case line, ok = <-stdout:
-		case line, ok = <-stderr:
-		}
-		if !ok {
-			return
-		}
-		logger.Info(line)
-	}
+	serverCancel() // for the linter
 }

internal/config/blocking.go
@@ -13,8 +13,7 @@ func getBlacklistSettings(reader *reader) (settings blacklist.BuilderSettings, e
 	if err != nil {
 		return settings, err
 	}
-	settings.BlockSurveillance, err = reader.env.OnOff("BLOCK_SURVEILLANCE", params.Default("off"),
-		params.RetroKeys([]string{"BLOCK_NSA"}, reader.onRetroActive))
+	settings.BlockSurveillance, err = reader.env.OnOff("BLOCK_SURVEILLANCE", params.Default("off"))
 	if err != nil {
 		return settings, err
 	}
@@ -30,23 +29,33 @@ func getBlacklistSettings(reader *reader) (settings blacklist.BuilderSettings, e
 	if err != nil {
 		return settings, err
 	}
-	settings.AddBlockedIPs, settings.AddBlockedIPNets, err = getBlockedIPs(reader)
+	settings.AddBlockedIPs, err = getBlockedIPs(reader)
 	if err != nil {
 		return settings, err
 	}
-	privateIPs, privateIPNets, err := getPrivateAddresses(reader)
+	settings.AddBlockedIPNets, err = getBlockedIPNets(reader)
 	if err != nil {
 		return settings, err
 	}
-	settings.AddBlockedIPs = append(settings.AddBlockedIPs, privateIPs...)
-	settings.AddBlockedIPNets = append(settings.AddBlockedIPNets, privateIPNets...)
+	rebindingProtection, err := reader.env.OnOff("REBINDING_PROTECTION", params.Default("on"))
+	if err != nil {
+		return settings, err
+	}
+	if rebindingProtection {
+		privateIPNets, err := getPrivateIPNets()
+		if err != nil {
+			return settings, err
+		}
+		settings.AddBlockedIPNets = append(settings.AddBlockedIPNets, privateIPNets...)
+	}
+
 	return settings, nil
 }
 
 // getAllowedHostnames obtains a list of hostnames to unblock from block lists
 // from the comma separated list for the environment variable UNBLOCK.
 func getAllowedHostnames(reader *reader) (hostnames []string, err error) {
-	hostnames, err = reader.env.CSV("UNBLOCK")
+	hostnames, err = reader.env.CSV("ALLOWED_HOSTNAMES")
 	if err != nil {
 		return nil, err
 	}
@@ -73,35 +82,66 @@ func getBlockedHostnames(reader *reader) (hostnames []string, err error) {
 	return hostnames, nil
 }
 
-// getBlockedIPs obtains a list of IP addresses and IP networks to block from
+// getBlockedIPs obtains a list of IP addresses to block from
 // the comma separated list for the environment variable BLOCK_IPS.
-func getBlockedIPs(reader *reader) (ips []net.IP, ipNets []*net.IPNet, err error) {
+func getBlockedIPs(reader *reader) (ips []net.IP, err error) {
 	values, err := reader.env.CSV("BLOCK_IPS")
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
-	ips, ipNets, err = convertStringsToIPs(values)
-	if err != nil {
-		return nil, nil, fmt.Errorf("invalid blocked IP string: %s", err)
-	}
-	return ips, ipNets, nil
-}
 
-func convertStringsToIPs(values []string) (ips []net.IP, ipNets []*net.IPNet, err error) {
-	ips = make([]net.IP, 0, len(values))
-	ipNets = make([]*net.IPNet, 0, len(values))
+	ips = make([]net.IP, len(values))
 	for _, value := range values {
 		ip := net.ParseIP(value)
-		if ip != nil {
-			ips = append(ips, ip)
-			continue
+		if ip == nil {
+			return nil, fmt.Errorf("invalid blocked IP: %s", err)
 		}
-		_, IPNet, err := net.ParseCIDR(value)
-		if err == nil && IPNet != nil {
-			ipNets = append(ipNets, IPNet)
-			continue
-		}
-		return nil, nil, fmt.Errorf("%s", value)
+		ips = append(ips, ip)
 	}
-	return ips, ipNets, nil
+
+	return ips, nil
+}
+
+// getBlockedIPNets obtains a list of IP networks (CIDR notation) to block from
+// the comma separated list for the environment variable BLOCK_IPNETS.
+func getBlockedIPNets(reader *reader) (ipNets []*net.IPNet, err error) {
+	values, err := reader.env.CSV("BLOCK_IPNETS")
+	if err != nil {
+		return nil, err
+	}
+
+	ipNets = make([]*net.IPNet, len(values))
+	for _, value := range values {
+		_, ipNet, err := net.ParseCIDR(value)
+		if err != nil {
+			return nil, fmt.Errorf("invalid blocked IP network CIDR: %s", err)
+		}
+		ipNets = append(ipNets, ipNet)
+	}
+
+	return ipNets, nil
+}
+
+func getPrivateIPNets() (privateIPNets []*net.IPNet, err error) {
+	privateCIDRs := []string{
+		"127.0.0.1/8",
+		"10.0.0.0/8",
+		"172.16.0.0/12",
+		"192.168.0.0/16",
+		"169.254.0.0/16",
+		"::1/128",
+		"fc00::/7",
+		// "fe80::/10", - TODO intepreted as 0.0.0.0/0
+		"::ffff:0:0/96",
+	}
+	privateIPNets = make([]*net.IPNet, len(privateCIDRs))
+	for i := range privateCIDRs {
+		_, ipNet, err := net.ParseCIDR(privateCIDRs[i])
+		if err != nil {
+			return nil, err
+		}
+		privateIPNets[i] = ipNet
+	}
+
+	return privateIPNets, nil
 }

qdm12/cloudflare-dns-server

cmd/main.go
@@ -17,11 +17,10 @@ import (
 	"github.com/qdm12/dns/internal/splash"
 	"github.com/qdm12/dns/pkg/blacklist"
 	"github.com/qdm12/dns/pkg/check"
+	"github.com/qdm12/dns/pkg/doh"
+	"github.com/qdm12/dns/pkg/dot"
 	"github.com/qdm12/dns/pkg/nameserver"
-	"github.com/qdm12/dns/pkg/unbound"
 	"github.com/qdm12/golibs/logging"
-	customOS "github.com/qdm12/golibs/os"
-	"github.com/qdm12/updated/pkg/dnscrypto"
 )
 
 var (
@@ -43,11 +42,10 @@ func main() {
 	args := os.Args
 	logger := logging.NewParent(logging.Settings{})
 	configReader := config.NewReader(logger)
-	osIntf := customOS.New()
 
 	errorCh := make(chan error)
 	go func() {
-		errorCh <- _main(ctx, buildInfo, args, logger, configReader, osIntf)
+		errorCh <- _main(ctx, buildInfo, args, logger, configReader)
 	}()
 
 	select {
@@ -78,8 +76,7 @@ func main() {
 }
 
 func _main(ctx context.Context, buildInfo models.BuildInformation,
-	args []string, logger logging.ParentLogger, configReader config.Reader,
-	os customOS.OS) error {
+	args []string, logger logging.ParentLogger, configReader config.Reader) error {
 	if health.IsClientMode(args) {
 		// Running the program in a separate instance through the Docker
 		// built-in healthcheck, in an ephemeral fashion to query the
@@ -97,30 +94,14 @@ func _main(ctx context.Context, buildInfo models.BuildInformation,
 
 	const clientTimeout = 15 * time.Second
 	client := &http.Client{Timeout: clientTimeout}
-	// Create configurators
-	dnsCrypto := dnscrypto.New(client, "", "") // TODO checksums for build
-	const unboundEtcDir = "/unbound"
-	const unboundPath = "/unbound/unbound"
-	const cacertsPath = "/unbound/ca-certificates.crt"
-	dnsConf := unbound.NewConfigurator(logger, os.OpenFile, dnsCrypto, unboundEtcDir, unboundPath, cacertsPath)
-
-	if len(args) > 1 && args[1] == "build" {
-		if err := dnsConf.SetupFiles(ctx); err != nil {
-			return err
-		}
-		return nil
-	}
-
-	version, err := dnsConf.Version(ctx)
-	if err != nil {
-		return err
-	}
-	logger.Info("Unbound version: %s", version)
 
 	settings, err := configReader.ReadSettings()
 	if err != nil {
 		return err
 	}
+	logger = logger.NewChild(logging.Settings{
+		Level: settings.LogLevel,
+	})
 	logger.Info("Settings summary:\n" + settings.String())
 
 	wg := &sync.WaitGroup{}
@@ -136,9 +117,9 @@ func _main(ctx context.Context, buildInfo models.BuildInformation,
 
 	localIP := net.IP{127, 0, 0, 1}
 	logger.Info("using DNS address %s internally", localIP.String())
-	nameserver.UseDNSInternally(localIP) // use Unbound
+	nameserver.UseDNSInternally(localIP) // use the DoT/DoH server
 	wg.Add(1)
-	go unboundRunLoop(ctx, wg, settings, logger, dnsConf, client, crashed)
+	go runLoop(ctx, wg, settings, logger, client, crashed)
 
 	select {
 	case <-ctx.Done():
@@ -149,8 +130,8 @@ func _main(ctx context.Context, buildInfo models.BuildInformation,
 	return err
 }
 
-func unboundRunLoop(ctx context.Context, wg *sync.WaitGroup, settings config.Settings, //nolint:gocognit
-	logger logging.Logger, dnsConf unbound.Configurator, client *http.Client, crashed chan<- error,
+func runLoop(ctx context.Context, wg *sync.WaitGroup, settings config.Settings,
+	logger logging.Logger, client *http.Client, crashed chan<- error,
 ) {
 	defer wg.Done()
 	defer logger.Info("unbound loop exited")
@@ -159,11 +140,9 @@ func unboundRunLoop(ctx context.Context, wg *sync.WaitGroup, settings config.Set
 	firstRun := true
 
 	var (
-		unboundCtx               context.Context
-		unboundCancel            context.CancelFunc
-		waitError                chan error
-		stdoutLines, stderrLines chan string
-		err                      error
+		serverCtx    context.Context
+		serverCancel context.CancelFunc
+		waitError    chan error
 	)
 
 	for ctx.Err() == nil {
@@ -172,12 +151,13 @@ func unboundRunLoop(ctx context.Context, wg *sync.WaitGroup, settings config.Set
 			timer.Reset(settings.UpdatePeriod)
 		}
 
+		serverSettings := dot.ServerSettings{
+			Resolver: settings.DoT.Resolver,
+			Port:     settings.DoT.Port,
+			Cache:    settings.DoT.Cache,
+		}
+
 		if !firstRun {
-			logger.Info("downloading DNSSEC root hints and named root")
-			if err := dnsConf.SetupFiles(ctx); err != nil {
-				logAndWait(ctx, logger, err)
-				continue
-			}
 			logger.Info("downloading and building DNS block lists")
 			blacklistBuilder := blacklist.NewBuilder(client)
 			blockedHostnames, blockedIPs, blockedIPPrefixes, errs :=
@@ -188,97 +168,61 @@ func unboundRunLoop(ctx context.Context, wg *sync.WaitGroup, settings config.Set
 			logger.Info("%d hostnames blocked overall", len(blockedHostnames))
 			logger.Info("%d IP addresses blocked overall", len(blockedIPs))
 			logger.Info("%d IP networks blocked overall", len(blockedIPPrefixes))
-			settings.Unbound.Blacklist = blacklist.Settings{
-				FqdnHostnames: blockedHostnames,
-				IPs:           blockedIPs,
-				IPPrefixes:    blockedIPPrefixes,
-			}
-		}
-
-		logger.Info("generating Unbound configuration")
-		if err := dnsConf.MakeUnboundConf(settings.Unbound); err != nil {
-			logAndWait(ctx, logger, err)
-			continue
+			serverSettings.Blacklist.IPs = blockedIPs
+			serverSettings.Blacklist.IPPrefixes = blockedIPPrefixes
+			serverSettings.Blacklist.BlockHostnames(blockedHostnames)
 		}
 
 		if !firstRun {
-			unboundCancel()
+			serverCancel()
 			<-waitError
 			close(waitError)
-			close(stdoutLines)
-			close(stderrLines)
 		}
-		unboundCtx, unboundCancel = context.WithCancel(ctx)
+		serverCtx, serverCancel = context.WithCancel(ctx)
 
-		logger.Info("starting unbound")
-		stdoutLines, stderrLines, waitError, err = dnsConf.Start(unboundCtx, settings.Unbound.VerbosityDetailsLevel)
-		if err != nil {
-			crashed <- err
-			break
+		var server models.Server
+		switch settings.UpstreamType {
+		case config.DoT:
+			server = dot.NewServer(serverCtx, logger, serverSettings)
+		case config.DoH:
+			server = doh.NewServer(serverCtx, logger, settings.DoH)
 		}
 
-		go logUnboundStreams(logger, stdoutLines, stderrLines)
+		logger.Info("starting DNS server")
+		waitError = make(chan error)
+		go server.Run(serverCtx, waitError)
 
 		if settings.CheckDNS {
 			if err := check.WaitForDNS(ctx, net.DefaultResolver); err != nil {
 				crashed <- err
-				break
+				serverCancel()
+				return
 			}
 		}
 
 		if firstRun {
-			logger.Info("restarting Unbound the first time to get updated files")
+			logger.Info("restarting DNS server the first time to get updated files")
 			firstRun = false
 			continue
 		}
 
 		select {
 		case <-timer.C:
-			logger.Info("planned restart of unbound")
+			logger.Info("planned periodic restart of DNS server")
 		case <-ctx.Done():
 			if !timer.Stop() {
 				<-timer.C
 			}
-			logger.Warn("context canceled: exiting unbound run loop")
+			logger.Warn("context canceled: exiting DNS server run loop")
 		case waitErr := <-waitError:
 			close(waitError)
-			close(stdoutLines)
-			close(stderrLines)
 			if !timer.Stop() {
 				<-timer.C
 			}
 			crashed <- waitErr
-			unboundCancel()
+			serverCancel()
 			return
 		}
 	}
-	unboundCancel()
-}
-
-func logAndWait(ctx context.Context, logger logging.Logger, err error) {
-	const wait = 10 * time.Second
-	logger.Error("%s, retrying in %s", err, wait)
-	timer := time.NewTimer(wait)
-	select {
-	case <-timer.C:
-	case <-ctx.Done():
-		if !timer.Stop() {
-			<-timer.C
-		}
-	}
-}
-
-func logUnboundStreams(logger logging.Logger, stdout, stderr <-chan string) {
-	var line string
-	var ok bool
-	for {
-		select {
-		case line, ok = <-stdout:
-		case line, ok = <-stderr:
-		}
-		if !ok {
-			return
-		}
-		logger.Info(line)
-	}
+	serverCancel() // for the linter
 }

internal/config/blocking.go
@@ -13,8 +13,7 @@ func getBlacklistSettings(reader *reader) (settings blacklist.BuilderSettings, e
 	if err != nil {
 		return settings, err
 	}
-	settings.BlockSurveillance, err = reader.env.OnOff("BLOCK_SURVEILLANCE", params.Default("off"),
-		params.RetroKeys([]string{"BLOCK_NSA"}, reader.onRetroActive))
+	settings.BlockSurveillance, err = reader.env.OnOff("BLOCK_SURVEILLANCE", params.Default("off"))
 	if err != nil {
 		return settings, err
 	}
@@ -30,23 +29,33 @@ func getBlacklistSettings(reader *reader) (settings blacklist.BuilderSettings, e
 	if err != nil {
 		return settings, err
 	}
-	settings.AddBlockedIPs, settings.AddBlockedIPPrefixes, err = getBlockedIPs(reader)
+	settings.AddBlockedIPs, err = getBlockedIPs(reader)
 	if err != nil {
 		return settings, err
 	}
-	privateIPs, privateIPPrefixes, err := getPrivateAddresses(reader)
+	settings.AddBlockedIPPrefixes, err = getBlockedIPPrefixes(reader)
 	if err != nil {
 		return settings, err
 	}
-	settings.AddBlockedIPs = append(settings.AddBlockedIPs, privateIPs...)
-	settings.AddBlockedIPPrefixes = append(settings.AddBlockedIPPrefixes, privateIPPrefixes...)
+	rebindingProtection, err := reader.env.OnOff("REBINDING_PROTECTION", params.Default("on"))
+	if err != nil {
+		return settings, err
+	}
+	if rebindingProtection {
+		privateIPPrefixes, err := getPrivateIPPrefixes()
+		if err != nil {
+			return settings, err
+		}
+		settings.AddBlockedIPPrefixes = append(settings.AddBlockedIPPrefixes, privateIPPrefixes...)
+	}
+
 	return settings, nil
 }
 
 // getAllowedHostnames obtains a list of hostnames to unblock from block lists
 // from the comma separated list for the environment variable UNBLOCK.
 func getAllowedHostnames(reader *reader) (hostnames []string, err error) {
-	hostnames, err = reader.env.CSV("UNBLOCK")
+	hostnames, err = reader.env.CSV("ALLOWED_HOSTNAMES")
 	if err != nil {
 		return nil, err
 	}
@@ -73,37 +82,65 @@ func getBlockedHostnames(reader *reader) (hostnames []string, err error) {
 	return hostnames, nil
 }
 
-// getBlockedIPs obtains a list of IP addresses and IP networks to block from
+// getBlockedIPs obtains a list of IP addresses to block from
 // the comma separated list for the environment variable BLOCK_IPS.
-func getBlockedIPs(reader *reader) (ips []netaddr.IP,
-	ipPrefixes []netaddr.IPPrefix, err error) {
+func getBlockedIPs(reader *reader) (ips []netaddr.IP, err error) {
 	values, err := reader.env.CSV("BLOCK_IPS")
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
-	ips, ipPrefixes, err = convertStringsToIPs(values)
-	if err != nil {
-		return nil, nil, fmt.Errorf("invalid blocked IP string: %s", err)
-	}
-	return ips, ipPrefixes, nil
-}
 
-func convertStringsToIPs(values []string) (ips []netaddr.IP,
-	ipPrefixes []netaddr.IPPrefix, err error) {
-	ips = make([]netaddr.IP, 0, len(values))
-	ipPrefixes = make([]netaddr.IPPrefix, 0, len(values))
+	ips = make([]netaddr.IP, len(values))
 	for _, value := range values {
 		ip, err := netaddr.ParseIP(value)
-		if err == nil {
-			ips = append(ips, ip)
-			continue
+		if err != nil {
+			return nil, fmt.Errorf("invalid blocked IP: %s", err)
 		}
-		ipPrefix, err := netaddr.ParseIPPrefix(value)
-		if err == nil {
-			ipPrefixes = append(ipPrefixes, ipPrefix)
-			continue
-		}
-		return nil, nil, fmt.Errorf("%s", value)
+		ips = append(ips, ip)
 	}
-	return ips, ipPrefixes, nil
+
+	return ips, nil
+}
+
+// getBlockedIPPrefixes obtains a list of IP networks (CIDR notation) to block from
+// the comma separated list for the environment variable BLOCK_IPNETS.
+func getBlockedIPPrefixes(reader *reader) (ipPrefixes []netaddr.IPPrefix, err error) {
+	values, err := reader.env.CSV("BLOCK_IPNETS")
+	if err != nil {
+		return nil, err
+	}
+
+	ipPrefixes = make([]netaddr.IPPrefix, len(values))
+	for _, value := range values {
+		ipPrefix, err := netaddr.ParseIPPrefix(value)
+		if err != nil {
+			return nil, fmt.Errorf("invalid blocked IP network CIDR: %s", err)
+		}
+		ipPrefixes = append(ipPrefixes, ipPrefix)
+	}
+
+	return ipPrefixes, nil
+}
+
+func getPrivateIPPrefixes() (privateIPPrefixes []netaddr.IPPrefix, err error) {
+	privateCIDRs := []string{
+		"127.0.0.1/8",
+		"10.0.0.0/8",
+		"172.16.0.0/12",
+		"192.168.0.0/16",
+		"169.254.0.0/16",
+		"::1/128",
+		"fc00::/7",
+		// "fe80::/10", - TODO intepreted as 0.0.0.0/0
+		"::ffff:0:0/96",
+	}
+	privateIPPrefixes = make([]netaddr.IPPrefix, len(privateCIDRs))
+	for i := range privateCIDRs {
+		privateIPPrefixes[i], err = netaddr.ParseIPPrefix(privateCIDRs[i])
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return privateIPPrefixes, nil
 }